55c5f3b0cc26c22741e10ffffe7874e93f90ec3e
SarahSchmidt <47662713+sschmidt0@users.noreply.github.com>
2021-03-08 15:32:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import Book from "./components/Book";
 import books from './data/books';
 
 export default () => {
-    let key = 0;
-    const bookComponents = books.map(book => <Book key={ ++key } title={ book.title } author={ book.author } />);
+    const bookComponents = books.map( (book, key) => <Book key={key } title={ book.title } author={ book.author } />);
 
     return (
         <div>
